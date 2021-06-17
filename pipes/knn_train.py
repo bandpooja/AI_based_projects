d5a82a3e2235fd0e49a4a1eca21b481c864eddd7
bandpooja <59486401+bandpooja@users.noreply.github.com>
2021-06-17 17:22:55 +0530

    decision tree

--- pipes/knn_train.py
+++ pipes/knn_train.py
@@ -1,11 +1,9 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import seaborn as sns
+import pandas as pd 
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
-
-seed=2021
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
 # Load in the data
 try:
@@ -32,28 +30,48 @@ train_score = regr.score(X_train, y_train) * 100
 test_score = regr.score(X_test, y_test) * 100
 
 # Write scores to a file
-with open("knn_metrics.txt", 'w') as outfile:
+with open("KNNmetrics.txt", 'w') as outfile:
         outfile.write("Training variance explained: %2.1f%%\n" % train_score)
-        outfile.write("Test variance explained: %2.1f%%\n" % test_score)
-
+        outfile.write("Test variance explained: %2.1f%%\n" %)
+        
 
 ##########################################
-############ PLOT RESIDUALS  #############
+##### PLOT FEATURE IMPORTANCE ############
 ##########################################
+# Calculate feature importance in random forest
+importances = regr.feature_importances_
+labels = df.columns
+feature_df = pd.DataFrame(list(zip(labels, importances)), columns = ["feature","importance"])
+feature_df = feature_df.sort_values(by='importance', ascending=False,)
+
 # image formatting
 axis_fs = 18 #fontsize
 title_fs = 22 #fontsize
 sns.set(style="whitegrid")
 
+ax = sns.barplot(x="importance", y="feature", data=feature_df)
+ax.set_xlabel('Importance', fontsize=axis_fs)
+ax.set_ylabel('Feature', fontsize=axis_fs)#ylabel
+ax.set_title('KNN\nfeature importance', fontsize = title_fs)
+
+plt.tight_layout()
+plt.savefig("feature_importance.png", dpi=120)
+plt.close()
+
+
+##########################################
+############ PLOT RESIDUALS  #############
+##########################################
+
 y_pred = regr.predict(X_test)
 y_jitter = y_test
 res_df = pd.DataFrame(list(zip(y_jitter, y_pred)), columns=["true", "pred"])
 
-ax = sns.scatterplot(x="true", y="pred", data=res_df)
+ax = sns.scatterplot(x="true", y="pred",data=res_df)
 ax.set_aspect('equal')
-ax.set_xlabel('True wine quality', fontsize=axis_fs)
-ax.set_ylabel('Predicted wine quality', fontsize=axis_fs)#ylabel
-ax.set_title('Residuals', fontsize=title_fs)
+ax.set_xlabel('True wine quality',fontsize = axis_fs) 
+ax.set_ylabel('Predicted wine quality', fontsize = axis_fs)#ylabel
+ax.set_title('Residuals', fontsize = title_fs)
 
 # Make it pretty- square aspect ratio
 ax.plot([1, 10], [1, 10], 'black', linewidth=1)
@@ -61,4 +79,4 @@ plt.ylim((2.5,8.5))
 plt.xlim((2.5,8.5))
 
 plt.tight_layout()
-plt.savefig("knn_residuals.png", dpi=120)
+plt.savefig("KNNresiduals.png",dpi=120) 
